3c61007f19457f95269d45c962358c2e74e085cf
deordiev-dm <dimadeordiev2002@gmail.com>
2024-08-23 14:36:22 +0300

    improved accessibility

--- slider-react-ts/src/App.tsx
+++ slider-react-ts/src/App.tsx
@@ -1,18 +1,34 @@
-import ImageSlider from "./ImageSlider"
+import ImageSlider from './ImageSlider';
 
-import img1 from "./images/dogs-01.jpg"
-import img2 from "./images/dogs-02.jpg"
-import img3 from "./images/dogs-03.jpg"
-import img4 from "./images/dogs-04.jpg"
-import img5 from "./images/dogs-05.jpg"
-import img6 from "./images/cats-01.jpg"
+import img1 from './images/dogs-01.jpg';
+import img2 from './images/dogs-02.jpg';
+import img3 from './images/dogs-03.jpg';
+import img4 from './images/dogs-04.jpg';
+import img5 from './images/dogs-05.jpg';
+import img6 from './images/cats-01.jpg';
 
-const IMAGES = [img1, img2, img3, img4, img5, img6]
-
-console.log(img1)
+const IMAGES = [
+  { url: img1, alt: 'Two black dogs in a bed' },
+  { url: img2, alt: 'Two black dogs sitting in a box' },
+  { url: img3, alt: 'Two brown dogs sitting on a bed' },
+  { url: img4, alt: 'Three white dogs on a grass hill with sunset in the background' },
+  { url: img5, alt: 'Huskies pulling a sled with aurora in the sky' },
+  { url: img6, alt: 'Two cute cats lying on a bench and looking into the camera' },
+];
 
 function App() {
-  return <ImageSlider imgUrls={IMAGES}/>
+  return (
+    <>
+      <ImageSlider imgData={IMAGES} />
+      <a
+        href='#'
+        style={{ marginTop: '2rem', display: 'inline-block' }}
+        id='after-image-slider-controls'
+      >
+        Try to tab through the page
+      </a>
+    </>
+  );
 }
 
-export default App
+export default App;
--- slider-react-ts/src/ImageSlider.tsx
+++ slider-react-ts/src/ImageSlider.tsx
@@ -1,63 +1,80 @@
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
-import { useState } from "react";
-import "./ImageSlider.css"
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import { useState } from 'react';
+import './ImageSlider.css';
 
 type ImageSliderProps = {
-  imgUrls: string[]
-}
+  imgData: {
+    url: string;
+    alt: string;
+  }[];
+};
 
-function ImageSlider({imgUrls} : ImageSliderProps) {
-  const [currentIndex, setCurrentIndex] = useState(0)
+function ImageSlider({ imgData }: ImageSliderProps) {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   function showPrevImage() {
-    setCurrentIndex(prevIndex => {
-      return prevIndex === 0 ? imgUrls.length - 1 : prevIndex - 1;
-    })
+    setCurrentIndex((prevIndex) => {
+      return prevIndex === 0 ? imgData.length - 1 : prevIndex - 1;
+    });
   }
 
   function showNextImage() {
-    setCurrentIndex(prevIndex => {
-      return prevIndex === imgUrls.length - 1 ? 0 : prevIndex + 1;
-    })
+    setCurrentIndex((prevIndex) => {
+      return prevIndex === imgData.length - 1 ? 0 : prevIndex + 1;
+    });
   }
 
-
   return (
-  <div className="slider">
-    <div className="slider-container">
-      { imgUrls.map((url) => {
-        return (
-        <img 
-          key={url} 
-          src={url} 
-          alt="" 
-          className="slider-image" 
-          style={{transform: `translate(${-100 * currentIndex}%)`}} 
-        />
-      )
-      }) }
-    </div>
-    <button onClick={showPrevImage} className="slider-btn slider-btn--prev">
-      <FaArrowAltCircleLeft />
-    </button>
-    <button onClick={showNextImage} className="slider-btn slider-btn--next" >
-    <FaArrowAltCircleRight />
-    </button>
-    <div className="slider-pagination">
-      {
-        imgUrls.map((_, index) => {
+    <section className='image-slider'>
+      <a
+        href='#after-image-slider-controls'
+        className='skip-link'
+      >
+        Skip Image Slider Controls
+      </a>
+      <div className='slider-container'>
+        {imgData.map((img, index) => {
           return (
-            <button 
-              key={index} 
-              onClick={() =>  setCurrentIndex(index)}
-              className={`${index === currentIndex ? "active" : ""}`}
-            >
-            </button>)
-        })
-      }
-    </div>
-  </div>
-  ); 
+            <img
+              key={img.url}
+              src={img.url}
+              alt={img.alt}
+              className='slider-image'
+              style={{ transform: `translate(${-100 * currentIndex}%)` }}
+              aria-hidden={index === currentIndex ? false : true}
+            />
+          );
+        })}
+      </div>
+      <button
+        onClick={showPrevImage}
+        className='slider-btn slider-btn--prev'
+        aria-label='Show Previous Image'
+      >
+        <FaArrowAltCircleLeft aria-hidden />
+      </button>
+      <button
+        onClick={showNextImage}
+        className='slider-btn slider-btn--next'
+        aria-label='Show Next Image'
+      >
+        <FaArrowAltCircleRight aria-hidden />
+      </button>
+      <div className='slider-pagination'>
+        {imgData.map((_, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+              className={`${index === currentIndex ? 'active' : ''}`}
+              aria-label={`Show Image ${index + 1}`}
+            ></button>
+          );
+        })}
+      </div>
+      <div id='after-image-slider-controls'></div>
+    </section>
+  );
 }
 
 export default ImageSlider;
